1a2833faeee969cdb083fba5aa94859d0ab963bf
Natasha Gurova <78147880+Tulenenok@users.noreply.github.com>
2022-01-03 23:32:50 +0300

    edit tree_node.h

--- Programming in C/template code/tree_node.h
+++ Programming in C/template code/tree_node.h
@@ -12,6 +12,20 @@ typedef struct tree_node
 } tree_node_t;
 
 tree_node_t *create_tree_node(void *data);
+tree_node_t *insert(tree_node_t *new_elem, tree_node_t *head, int (*cmp_data)(void *, void *));
+void print_tree_node(tree_node_t *node, void *arg);
 
+tree_node_t *find(tree_node_t *node, tree_node_t *head, int (*cmp_data)(void *, void *));
+tree_node_t *find_with_while(tree_node_t *node, tree_node_t *head, int (*cmp_data)(void *, void *));
+
+void pre_order(tree_node_t *head, void (*action)(tree_node_t *, void *), void *arg);
+void in_order(tree_node_t *head, void (*action)(tree_node_t *, void *), void *arg);
+void post_order(tree_node_t *head, void (*action)(tree_node_t *, void *), void *arg);
+
+void to_dot_help_func(struct tree_node *tree, void *arg);
+void export_to_dot(FILE *f, const char *tree_name, tree_node_t *tree);
+
+void free_tree_node(tree_node_t *node, void *arg);
+void free_tree(tree_node_t *head);
 
 #endif
