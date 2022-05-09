93e80e5d5a587a9fb93090870c3de82711d8661e
Gurova Natalia <gurovana@student.bmstu.ru>
2022-05-09 19:00:30 +0300

    Delete CG

--- Сomputer graphics/lab_02/shared/SharedSettings.py
+++
@@ -1,6 +0,0 @@
-class SharedSettings:
-    FILE_PREVIOUS_VERSION_P_P = "shared/previousVersionPP.bin"
-    FILE_PREVIOUS_VERSION_P = "shared/previousVersionP.bin"
-    FILE_PREVIOUS_VERSION = "shared/previousVersion.bin"
-    FILE_CURRENT_VERSION = "shared/currentVersion.bin"
-
